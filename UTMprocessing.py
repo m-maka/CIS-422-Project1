ab18f8111298e8e9ad1a3e528787018e9f0202ee
maguirr4 <maguirr4@cs.uoregon.edu>
2020-10-05 23:18:03 -0700

    Set up the file to begin work

--- UTMprocessing.py
+++ UTMprocessing.py
@@ -1,2 +1,19 @@
-# Placeholder comment for initial UTM commit
+'''
+Authors: Max Aguirre, Brandon Bower, Mapuana Maka
+
+This file converts pairs of lattitude and longitude values into the UTM 
+Coordinate System. It also calculates the distance between UTM points.
+
+'''
+
+import utm
+import geopy.distance
+
+
+
+def main:
+	# Main function that will do a thing
+
+if __name__ == '__main__':
+	main()
 
