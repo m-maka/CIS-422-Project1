5a6890df8169c823d1fc19a543fe0ed23989119c
BowerB <brandonkbower@gmail.com>
2020-10-14 21:03:35 -0700

    Now divides tracks into segments with a start, end, and up to 10 waypoints

--- src/findturns.py
+++ src/findturns.py
@@ -3,35 +3,58 @@ Authors: Brandon Bower along with Max Aguirre and Mapuana Maka
 
 '''
 
-import requests
+import googlemaps
 
-def getloc(key, lat, long):
-        query = {'point.lat' : str(lat), 'point.lon' : str(long), 'size' : 1}
-        response = requests.get(f"https://api.openrouteservice.org/geocode/reverse?api_key={key}",
-                                params=query)
-        jsonResponse = response.json()
-        name = ''
-        for dictionary in jsonResponse['features']:
-            if 'properties' in dictionary.keys():
-                print(dictionary['properties']['label'])
-                name = dictionary['properties']['label']
-        return name
+def turnDetect(client, latlongs):
+    #result = client.directions(latlongs[0], latlongs[1])
+    #print(result)
+
+    return  None
 
 
-def turndetect(key, latlongs):
-    assert len(latlongs) != 0, "Your gpx file has no lat long points"
-
-    start = getloc(key, latlongs[0][0], latlongs[0][1])
+def createSegments(latlongs):
+    length = len(latlongs)
+    segNumb = (length // 12) + 1
+    index = 0
+    segments = []
+    for segment in range(segNumb):
+        print(f'start (index {index})')
+        #segments.append({'start' : latlongs[index]})
+        waypoint = 1
+        for index in range(index, min(index + 10,length - 2)):
+            index += 1
+            print(f"w{waypoint}, (index {index})")
+            waypoint += 1
+        if waypoint:
+            index += 1
+        print(f'end (index {index})')
+        #segments[segment]['end'] = latlongs[index] 
     
     
     
+
+
 
 def main():
-    key = "you api key here for testing"
-    latlongs = [(44.0451248, -123.0756419)]
-    turndetect(key, latlongs)
+    '''
+    latlongs = [(44.2185374, -123.2125012), (44.21857, -123.21163),
+                (44.21898, -123.21158), (44.2190527, -123.2089076),
+                (44.2225, -123.20905), (44.2223507, -123.2147348),
+                (44.22415, -123.21471), (44.22468, -123.21394),
+                (44.2264858, -123.2139206)]
+    '''
+    latlongs = [(44.587662, -123.256691), (44.587395, -123.262154), (44.587502, -123.262497),
+                (44.596886, -123.262482), (44.598633, -123.26149), (44.599155, -123.261673),
+                (44.60146, -123.263374), (44.602081, -123.263222),(44.602959, -123.262421),
+                (44.603649, -123.262161), (44.612709, -123.262253), (44.613117, -123.267418),
+                (44.613689, -123.268028), (44.616093, -123.268036), (44.616718, -123.268425),
+                (44.616905, -123.26915), (44.616951, -123.274727), (44.6172449, -123.275269),
+                (44.618179, -123.275856), (44.618835, -123.275887), (44.6216579, -123.275101),
+                (44.622791, -123.274361),(44.62421, -123.27401), (44.626221, -123.27269)]
+    #gmaps = googlemaps.Client(key='Your Key Here')
+    segments = createSegments(latlongs)
+    #turnDetect(gmaps, segments)
 
 
 if __name__ == '__main__':
-
     main()
